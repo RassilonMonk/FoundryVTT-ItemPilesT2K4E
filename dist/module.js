697de4447c010656db36ac025cc47b7a587af5dc
RassilonMonk <rassilonmonk@gmail.com>
2025-06-10 11:42:29 -0400

    Added base code from old ItemPiles system data for t2k4e

---
+++ dist/module.js
@@ -0,0 +1,41 @@
+Hooks.once("item-piles-ready", async () => {
+
+    "VERSION": "1.0.1",
+
+	const baseConfig = {
+		// These keys and setting are unlikely to ever change
+
+	// The actor class type is the type of actor that will be used for the default item pile actor that is created on first item drop.
+	"ACTOR_CLASS_TYPE": "character",
+
+	// The item class type is the type of item that will be used for the default loot item
+	"ITEM_CLASS_LOOT_TYPE": "",
+
+	// The item class type is the type of item that will be used for the default weapon item
+	"ITEM_CLASS_WEAPON_TYPE": "", 
+
+	// The item class type is the type of item that will be used for the default equipment item
+	"ITEM_CLASS_EQUIPMENT_TYPE": "",
+
+	// The item quantity attribute is the path to the attribute on items that denote how many of that item that exists
+	"ITEM_QUANTITY_ATTRIBUTE": "system.qty",
+
+	// The item price attribute is the path to the attribute on each item that determine how much it costs
+	"ITEM_PRICE_ATTRIBUTE": "system.price",
+
+	// Item types and the filters actively remove items from the item pile inventory UI that users cannot loot, such as spells, feats, and classes
+	"ITEM_FILTERS": [
+		{
+			"path": "type",
+			"filters": "specialty,injury"
+		}
+	],
+
+	// Item similarities determines how item piles detect similarities and differences in the system
+	"ITEM_SIMILARITIES": ["name", "type"],
+
+	// Currencies in item piles is a versatile system that can accept actor attributes (a number field on the actor's sheet) or items (actual items in their inventory)
+	// In the case of attributes, the path is relative to the "actor.system"
+	// In the case of items, it is recommended you export the item with `.toObject()` and strip out any module data
+	"CURRENCIES": [],
+});
